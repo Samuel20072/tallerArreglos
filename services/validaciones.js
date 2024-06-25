d275d0e1d4cde934f9ce6070030e07e1c52fe467
Samuel20072 <161377949+Samuel20072@users.noreply.github.com>
2024-06-25 17:50:28 -0500

    Delete services directory

--- services/validaciones.js
+++
@@ -1,9 +0,0 @@
-export function validNum(value) {
-    if (!isNaN(parseFloat(value)) && isFinite(value)) {
-        return "Número";
-    } else if (/^[a-zA-Z]+$/.test(value)) {
-        return "Letra";
-    } else {
-        return "No se pudo identificar";
-    }
-}
